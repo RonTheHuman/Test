d086db69e0e3aa84b064580a1bbec4acc7c7f6aa
RonTheHuman <1ron.kokotov@gmail.com>
2022-01-25 15:54:10 +0200

    idk

--- python_f.py
+++ python_f.py
@@ -1,11 +1,6 @@
 print("a")
 print("b")
 print("c")
-<<<<<<< HEAD
-
-print("completely new content")
-print("They move me in boxes")
-=======
 print("d")
 print("completely new content")
->>>>>>> dev2
+print("They move me in boxes")
