d910e4c5ad455dba9d5d4f717c3ed95f85b01083
RonTheHuman <1ron.kokotov@gmail.com>
2022-01-25 18:49:04 +0200

    mmmmmmm

--- python_f.py
+++ python_f.py
@@ -17,6 +17,6 @@ print("They move me in boxes!")
 print("Now I know where the ducks go")
 print("They move me in boxes!")
 print("They move me in boxes!")
-
+#texty?
 print("We are now developing a way to sort ducks from geese")
 # texty
